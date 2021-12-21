caf91b27fcf6d326fffa31e550f46b821bc861ee
quancie <deguzman252002@gmail.com>
2021-12-21 09:05:07 +0800

    .

--- AniQuinch/script.js
+++ AniQuinch/script.js
@@ -10,13 +10,13 @@ btn1.addEventListener('click', function(){
     if(counter <= 0) return;
     carousel.style.transition = "transform 1s ease-in-out";
     counter--;
-    carousel.style.transform = 'translateX(' + (-1920 * counter) + 'px)';
+    carousel.style.transform = 'translateX(' + (-1440 * counter) + 'px)';
 });
 btn2.addEventListener('click', function(){
     if(counter >= images.length - 1) return;
     carousel.style.transition = "transform 1s ease-in-out";
     counter++;
-    carousel.style.transform = 'translateX(' + (-1920 * counter) + 'px)';
+    carousel.style.transform = 'translateX(' + (-1440 * counter) + 'px)';
 });
 
 // Sidebar Function
@@ -35,4 +35,49 @@ menu.addEventListener('click', function(){
         sidebar.style.left = "-350px";
     }
     toggleMenu = !toggleMenu;
+})
+
+// Navbar Scrolldown Function
+
+const navbar = document.querySelector(".navigation-container");
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 1000)
+    {
+        navbar.style.backgroundColor = "#f85d83";
+        navbar.style.transition = ".75s";
+        
+    }
+    else if(window.scrollY < 1000)
+    {
+        navbar.style.backgroundColor = "#fd709367";
+        navbar.style.transition = ".75s";
+    }
+})
+
+// Featured Products
+
+const featuredTitle = document.querySelector(".featured-title")
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 2000){
+        featuredTitle.style.textShadow = "0px 0px 50px #FFFFFF, 0px 0px 50px #FFFFFF"
+    }
+    else if(window.scrollY < 2000)
+    {
+        featuredTitle.style.textShadow = "none"
+    }
+})
+
+// About
+const about = document.querySelector(".about")
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 1000){
+        about.style.marginTop = "10rem"
+    }
+    else if(window.scrollY < 1000)
+    {
+        about.style.marginTop = "5rem"
+    }
 })
